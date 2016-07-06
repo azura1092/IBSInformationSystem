a5553beb25469a8926884bc528ee93084270cfd6
Jun Mitra <Jun Mitra>
2016-07-07 01:25:06 +0800

    Added repopulation admin

--- resources/views/facultyandstaff/admin/add-staff-employee.blade.php
+++ resources/views/facultyandstaff/admin/add-staff-employee.blade.php
@@ -1,14 +1,7 @@
 @extends('facultyandstaff.admin.dash-admin')
 
 @section('content')
-	@if($status == 0)
-
-		<div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            Record cannot be added. Employee number already exists.
-    	</div>
-
-	@elseif($status == 2)
+	@if($status == 2)
 
 		<div class="alert alert-success alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
@@ -32,27 +25,39 @@
 								<div class="form-group">
 									<label class="col-sm-3 control-label">Employee Number</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="employeeNumber" id="employeeNumber" type="text" value="" required>
+										<input class="form-control" name="employeeNumber" id="employeeNumber" type="text" value="{{old('employeeNumber')}}" required>
+										@if($errors->has('employeeNumber'))
+											<li class="list-group-item list-group-item-danger">Please type a valid employee number</li>
+										@endif
 									</div>
 								</div>
 								<div class="form-group">
 									<label class="col-sm-3 control-label">First Name</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="firstName" id="firstName" type="text" value="" required>
+										<input class="form-control" name="firstName" id="firstName" type="text" value="{{old('firstName')}}" required>
+										@if($errors->has('firstName'))
+											<li class="list-group-item list-group-item-danger">Please check your input</li>
+										@endif
 									</div>
 								</div>	
 
 								<div class="form-group">
 									<label class="col-sm-3 control-label">Middle Name</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="middleName" id="middleName" type="text" value="" required>
+										<input class="form-control" name="middleName" id="middleName" type="text" value="{{old('middleName')}}" required>
+										@if($errors->has('middleName'))
+											<li class="list-group-item list-group-item-danger">Please check your input</li>
+										@endif
 									</div>
 								</div>
 
 								<div class="form-group">
 									<label class="col-sm-3 control-label">Last Name</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="lastName" id="lastName" type="text" value="" required>
+										<input class="form-control" name="lastName" id="lastName" type="text" value="{{old('lastName')}}" required>
+										@if($errors->has('lastName'))
+											<li class="list-group-item list-group-item-danger">Please check your input</li>
+										@endif
 									</div>
 								</div>
 
@@ -62,9 +67,12 @@
 										<select class="form-control" name="position" id="position" required>
 											<option selected disabled></option>
 											@foreach($positions as $p)
-												<option>{{ $p->positionTitle }}</option>
+												<option value="{{ $p->positionTitle }}" @if (Input::old('position') == $p->positionTitle) selected="selected" @endif>{{ $p->positionTitle }}</option>
 											@endforeach
-										</select>		
+										</select>	
+										@if($errors->has('position'))
+											<li class="list-group-item list-group-item-danger">Please check your input</li>
+										@endif	
 									</div>
 								</div>
 
@@ -73,49 +81,73 @@
 									<div class="col-sm-7">
 										<select class="form-control" name="sex" id="sex" required>
 											<option selected disabled></option>
-											<option name="Female" value="Female">Female</option>
-											<option name="Male" value="Male">Male</option>
+											<option name="Female" value="Female" @if (Input::old('sex') == 'Female') selected="selected" @endif>Female</option>
+											<option name="Male" value="Male" @if (Input::old('sex') == 'Male') selected="selected" @endif>Male</option>
 										</select>
+										@if($errors->has('sex'))
+											<li class="list-group-item list-group-item-danger">Please check your input</li>
+										@endif
 									</div>
 								</div>
 
 								<div class="form-group">
 									<label class="col-sm-3 control-label">Birthdate</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="birthdate" id="birthdate" type="date" value="" required>
+										<input class="form-control" name="birthdate" id="birthdate" type="date" value="{{old('birthdate')}}" required>
+										@if($errors->has('birthdate'))
+											<li class="list-group-item list-group-item-danger">Please input a valid birthdate.</li>
+										@endif
 									</div>
 								</div>
 
 								<div class="form-group">
 									<label class="col-sm-3 control-label">Contact Number</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="contactNumber" id="contactNumber" type="number" value="" required>
+										<input class="form-control" name="contactNumber" id="contactNumber" type="text" value="{{old('contactNumber')}}" required>
+										@if($errors->has('contactNumber'))
+											<li class="list-group-item list-group-item-danger">Please input a valid contact number.</li>
+										@endif
 									</div>
 								</div>
 
 								<div class="form-group">
 									<label class="col-sm-3 control-label">Email Address</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="emailAddress" id="emailAddress" type="email" value="" required>
+										<input class="form-control" name="emailAddress" id="emailAddress" type="email" value="{{old('emailAddress')}}" required>
+										@if($errors->has('emailAddress'))
+											<li class="list-group-item list-group-item-danger">Please input a valid email address</li>
+										@endif
 									</div>
 								</div>
 								
 								<div class="form-group">
 									<label class="col-sm-3 control-label">Current Address</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="currentAddress" id="currentAddress" type="text" value="" required>
+										<textarea class="form-control" name="currentAddress" id="currentAddress" required>{{{old('currentAddress')}}}</textarea>
+										@if($errors->has('currentAddress'))
+											<li class="list-group-item list-group-item-danger">Please check your input</li>
+										@endif
 									</div>
 								</div>
 
 								<div class="form-group">
 									<label class="col-sm-3 control-label">Permanent Address</label>
 									<div class="col-sm-7">
-										<input class="form-control" name="permanentAddress" id="permanentAddress" type="text" value="" required>
+										<textarea class="form-control" name="permanentAddress" id="permanentAddress" required>{{{old('lastName')}}}</textarea>
+										@if($errors->has('permanentAddress'))
+											<li class="list-group-item list-group-item-danger">Please check your input</li>
+										@endif
 									</div>
 								</div>
 
 								<div class="form-group">
 									<div class="col-sm-10">
+										&nbsp;
+									</div>
+								</div>
+								
+								<div class="form-group">
+									<div class="col-sm-3">
 										<button type="submit" class="btn btn-success btn-block">Save</button>
 									</div>
 								</div>
